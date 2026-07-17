8b33d21c9d5c5caa09939664156aa1f846143c2d
Melga1502 <melgarejojosep@gmail.com>
2026-07-17 15:17:48 -0500

    Delete Whoop.HardwareTelemetry.Platform.u202315165 directory

--- Whoop.HardwareTelemetry.Platform.u202315165/Hardware/Domain/Model/Queries/GetAllDevicesQuery.cs
+++
@@ -1,7 +0,0 @@
-namespace Whoop.HardwareTelemetry.Platform.u202315165.Hardware.Domain.Model.Queries;
-
-/// <summary>
-///     Query used to request all devices.
-/// </summary>
-/// <remarks>Josep Eliu Melgarejo Quiroz</remarks>
-public record GetAllDevicesQuery;
--- Whoop.HardwareTelemetry.Platform.u202315165/Shared/Resources/Errors/ErrorMessages.cs
+++
@@ -1,9 +0,0 @@
-namespace Whoop.HardwareTelemetry.Platform.u202315165.Shared.Resources.Errors;
-
-/// <summary>
-///     Marker class for shared error message resources.
-/// </summary>
-/// <remarks>Josep Eliu Melgarejo Quiroz</remarks>
-public class ErrorMessages
-{
-}
